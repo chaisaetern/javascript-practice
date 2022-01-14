414f7255e6ad89a26b4e68ed411536aa53e84f3f
Chai Saetern <chaisaetern530@gmail.com>
2022-01-13 20:33:36 -0800

    Update script.js

--- day-2/js/script.js
+++ day-2/js/script.js
@@ -1,8 +1,6 @@
-let str = "Supercalifragilisticexpialidocious";
-let lengthOfStr = "Supercalifragilisticexpialidocious".length;
-console.log(str, lengthOfStr);
+console.log("Supercalifragilisticexpialidocious".length);
 
 console.log(500, typeof 500);
 
-let bigIntNumber = 10000000000n;
-console.log(bigIntNumber, typeof bigIntNumber);
+let bigNumber = 10000000000n;
+console.log(bigNumber, typeof bigNumber);
